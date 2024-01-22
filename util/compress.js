fea149e654d62e91ee5257278708c06087d49322
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-01-22 23:51:40 +0700

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -1,34 +1,34 @@
 const sharp = require("sharp");
 
-function resizeDanCompress(input, webp, grayscale, quality, originSize, maxWidth, maxHeight) {
-  const format = webp ? "webp" : "jpeg";
+function compress(input, webp, grayscale, quality, originSize) {
+	const format = webp ? "webp" : "jpeg";
 
-  return sharp(input)
-    .grayscale(grayscale)
-    .resize({ width: maxWidth, height: maxHeight, fit: 'inside' }) // Logika resize gambar
-    .toFormat(format, {
-      quality: quality,
-      progressive: true,
-      optimizeScans: true,
-    })
-    .toBuffer({ resolveWithObject: true })
-    .then(({ data: output, info }) => {
-      return {
-        err: null,
-        headers: {
-          "content-type": `image/${format}`,
-          "content-length": info.size,
-          "x-original-size": originSize,
-          "x-bytes-saved": originSize - info.size,
-        },
-        output: output,
-      };
-    })
-    .catch((err) => {
-      return {
-        err: err,
-      };
-    });
+	return sharp(input)
+		.grayscale(grayscale)
+		.toFormat(format, {
+			quality: quality,
+			progressive: true,
+			optimizeScans: true
+		})
+		.toBuffer({resolveWithObject: true})
+		.then(({data: output,info}) => {	// this way we can also get the info about output image, like height, width
+		// .toBuffer()
+		// .then( output => {
+			return {
+				err: null,
+				headers: {
+					"content-type": `image/${format}`,
+					"content-length": info.size,
+					"x-original-size": originSize,
+					"x-bytes-saved": originSize - info.size,
+				},
+				output: output
+			};
+		}).catch(err => {
+			return {
+				err: err
+			};
+		});
 }
 
-module.exports = resizeDanCompress;
+module.exports = compress;
